c56ac46d1234cd2b6c70bf6dac520b11cde96a48
Beerensd <beerensd@mail.gvsu.edu>
2015-04-09 15:29:14 -0400

    Jenkins Build Test
    Testing if Jenkins is currently building correctly

---
+++ DiscoverWebService/DiscoveryServiceWebAPI/Controllers/PurchasesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk;
+using System.Xml.Linq;
+using System.Text;
+namespace DiscoveryServiceWebAPI.Controllers
+{
+    public class PurchasesController : ApiController
+    {
+        public EntityCollection Get()
+        {
+            CrmConnection con = new CrmConnection("CRM");
+            IOrganizationService serviceTest = new OrganizationService(con);
+
+            QueryExpression query = new QueryExpression("contact");
+            query.ColumnSet.AllColumns = true;
+            
+            EntityCollection result = serviceTest.RetrieveMultiple(query);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            XElement xmlElements;
+            
+            foreach (Entity test in result.Entities)
+            {
+                xmlElements = new XElement("Contact", test.Attributes.Select(i => new XElement(i.Key.ToString(), i.Value.ToString())));
+                stringBuilder.Append(xmlElements.ToString());
+
+            }
+            return result;
+        }
+       
+    }
+}
